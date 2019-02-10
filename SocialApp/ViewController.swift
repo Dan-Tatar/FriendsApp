e8213c26df906dfec67069fae6c46d52bf6bcca8
Dan Tatar <dan.tatar@yahoo.com>
2019-02-10 12:49:55 +0000

    Initial Commit

---
+++ SocialApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SocialApp
+//
+//  Created by Dan  Tatar on 10/02/2019.
+//  Copyright © 2019 Dany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+  let url =  " https://www.hackingwithswift.com/samples/friendface.json"
+
+}
+
